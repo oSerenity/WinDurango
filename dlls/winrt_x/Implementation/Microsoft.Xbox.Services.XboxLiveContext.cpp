a4f6bfcb05899fd73d1e57c483c144447e86af09
Tyler Jaacks <TylerJaacks@Gmail.com>
2025-03-02 23:07:42 -0600

    Enhance leaderboard functionality and logging
    - Added a Boolean resource entry for "gamertag" in `WinDurango.sln.DotSettings`.
    - Cleaned up comments in generated files to prevent confusion.
    - Implemented logging and unimplemented function handling in `LeaderboardColumn`, `LeaderboardQuery`, `LeaderboardResult`, `LeaderboardRow`, and `LeaderboardService`.
    - Simplified return types in `Leaderboard` classes by removing namespace prefixes.
    - Introduced private member variables in `LeaderboardQuery`, `LeaderboardResult`, and `LeaderboardRow` for state management.
    - Updated `XboxLiveContext` methods to use logging instead of print statements for improved traceability.

--- dlls/winrt_x/Implementation/Microsoft.Xbox.Services.XboxLiveContext.cpp
+++ dlls/winrt_x/Implementation/Microsoft.Xbox.Services.XboxLiveContext.cpp
@@ -11,142 +11,191 @@ namespace winrt::Microsoft::Xbox::Services::implementation
 {
     XboxLiveContext::XboxLiveContext(Windows::Xbox::System::User const& user)
     {
-        printf("[Microsoft::Xbox::Services::XboxLiveContext] Constructor called\n");
-        //m_MultiplayerService = winrt::make<Multiplayer::implementation::MultiplayerService>();
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
     }
 
     Windows::Xbox::System::User XboxLiveContext::User()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [User] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Social::ProfileService XboxLiveContext::ProfileService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [ProfileService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Social::SocialService XboxLiveContext::SocialService()
     {
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         return winrt::make<Social::implementation::SocialService>( );
     }
 
     Social::ReputationService XboxLiveContext::ReputationService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [ReputationService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Achievements::AchievementService XboxLiveContext::AchievementService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [AchievementService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Leaderboard::LeaderboardService XboxLiveContext::LeaderboardService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [LeaderboardService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     UserStatistics::UserStatisticsService XboxLiveContext::UserStatisticsService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [UserStatisticsService] THIS FUNCTION IS STUBBED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
 		return winrt::make<UserStatistics::implementation::UserStatisticsService>( );
     }
 
     Matchmaking::MatchmakingService XboxLiveContext::MatchmakingService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [MatchmakingService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Multiplayer::MultiplayerService XboxLiveContext::MultiplayerService()
     {
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         return winrt::make<Multiplayer::implementation::MultiplayerService>( );
     }
 
     Tournaments::TournamentService XboxLiveContext::TournamentService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [TournamentService] NOT IMPLEMENTED !!!!\n");
-        throw hresult_not_implemented();
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
+    	throw hresult_not_implemented();
     }
 
     RealTimeActivity::RealTimeActivityService XboxLiveContext::RealTimeActivityService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [RealTimeActivityService] NOT IMPLEMENTED !!!!\n");
-        throw hresult_not_implemented();
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
+    	throw hresult_not_implemented();
     }
 
     Presence::PresenceService XboxLiveContext::PresenceService()
     {
-		return winrt::make<Presence::implementation::PresenceService>( );
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
+		return winrt::make<Presence::implementation::PresenceService>();
     }
 
     GameServerPlatform::GameServerPlatformService XboxLiveContext::GameServerPlatformService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [GameServerPlatformService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     TitleStorage::TitleStorageService XboxLiveContext::TitleStorageService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [TitleStorageService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Privacy::PrivacyService XboxLiveContext::PrivacyService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [PrivacyService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     System::StringService XboxLiveContext::StringService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [StringService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     ContextualSearch::ContextualSearchService XboxLiveContext::ContextualSearchService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [ContextualSearchService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Clubs::ClubsService XboxLiveContext::ClubsService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [ClubsService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Marketplace::CatalogService XboxLiveContext::CatalogService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [CatalogService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     Marketplace::InventoryService XboxLiveContext::InventoryService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [InventoryService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     EntertainmentProfile::EntertainmentProfileListService XboxLiveContext::EntertainmentProfileListService()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [EntertainmentProfileListService] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     XboxLiveContextSettings XboxLiveContext::Settings()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [Settings] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 
     XboxLiveAppConfiguration XboxLiveContext::AppConfig()
     {
-        printf("!!!!! Microsoft.Xbox.Services.XboxLiveContext [AppConfig] NOT IMPLEMENTED !!!!\n");
+        LOG_FUNCTION_NAME();
+        PRINT_UNIMPLEMENTED_FUNCTION();
+
         throw hresult_not_implemented();
     }
 }
